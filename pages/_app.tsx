2062c8cca85e82746069e01787e42f71c5b2a201
masac <msktjm98@gmail.com>
2023-05-17 19:00:37 +0900

    firebase 初期設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+/**
+ * 出来ること
+ ページ移動間の固定レイアウト
+ componentsDidCatchを使用下カスタムエラーハンドリング
+ 状態管理ライブラリとの結合
+ 
+ 全ページで必要な挙動を書ける場所なので、他にも広告まわりの関数の実行や、NProgressなどのローディングを設定したりしています。
+ 注意すべき点はNext.jsが用意するページコンポーネントと同じ挙動を取るので、_app.jsはサーバーサイドでレンダリング
+ （getInitialPropsの実行を含む）され、ライフサイクルのイベントはクライアントサイドでも実行されます。
+ */
+
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
