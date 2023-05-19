0ba1728159b9879c246c49af43f4a10ef407f113
masac <msktjm98@gmail.com>
2023-05-19 18:38:00 +0900

    ヘッダー作成　drawer付けただけ

---
+++ pages/posts/header.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  HStack,
+  Input,
+  Spacer,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React from "react";
+
+const Header = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = React.useRef()
+  return (
+    <HStack>
+      <Box>アプリ名</Box>
+      <Spacer />
+      <Button ref={btnRef} colorScheme='teal' onClick={onOpen}>
+        Open
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>
+            <Input placeholder='Type here...' />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant='outline' mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme='blue'>Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </HStack>
+  );
+};
+
+export default Header;
