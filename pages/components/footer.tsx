d50d09ec5a5faf1c4b1886e2859d5e209950f18e
masac <msktjm98@gmail.com>
2023-05-31 15:33:21 +0900

    フッター修正

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -3,10 +3,12 @@ import { chakra, Box, Stack, Container } from "@chakra-ui/react";
 const Footer = () => {
   return (
     <chakra.footer bgColor="gray.100">
-      <Container></Container>
-      <Container>
-        <Box>会社概要</Box>
+      <Container p='10px'>
+        <Box>サイトマップ</Box>
+        <Box>リンク</Box>
         <Box>お問い合わせ</Box>
+        <Box>ご利用に際して</Box>
+        <Box>©2023</Box>
       </Container>
     </chakra.footer>
   );
