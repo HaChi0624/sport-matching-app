6e88aa1d44cae4b826002cd74dafd96c73ae358a
masac <msktjm98@gmail.com>
2023-06-07 22:11:52 +0900

    形だけ

--- pages/posts/search.tsx
+++ pages/posts/search.tsx
@@ -1,9 +1,60 @@
-const myProfilePage = () => {
+import {
+  Box,
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+const search = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <>
-      <p>相手を探すページ</p>
+      <Box>
+        <Button onClick={onOpen}>好きな球団から</Button>
+        <p>好きな球団を選択する→相手一覧</p>
+
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <p>パ・リーグ</p>
+              <Button>オリックス</Button>
+              <Button>ソフトバンク</Button>
+              <Button>西武</Button>
+              <Button>楽天</Button>
+              <Button>ロッテ</Button>
+              <Button>日本ハム</Button>
+              <p>セ・リーグ</p>
+              <Button>ヤクルト</Button>
+              <Button>DeNA</Button>
+              <Button>阪神</Button>
+              <Button>巨人</Button>
+              <Button>広島</Button>
+              <Button>中日</Button>
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={onClose}>
+                Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </Box>
     </>
   );
 };
 
-export default myProfilePage;
+export default search;
+
+//一括で検索+下に一覧表示
+
+//検索方法を分ける
+//好きな球団or 行きたい日程
