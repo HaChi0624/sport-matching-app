4b3447ead81163f5fe23ecf9a6b1cf8c54be373f
masac <msktjm98@gmail.com>
2023-06-18 07:33:37 +0900

    送信機能作成

--- pages/posts/chat.tsx
+++ pages/posts/chat.tsx
@@ -1,7 +1,44 @@
+import {
+  Button,
+  chakra,
+  Container,
+  Heading,
+  Input,
+  Spacer,
+} from "@chakra-ui/react";
+
+import { db } from "@/firebase/firebase";
+import { FirebaseError } from "@firebase/util";
+import { FormEvent, useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+
 const chat = () => {
+  const [message, setMessage] = useState<string>("");
+
+  const handleSendMessage = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await addDoc(collection(db, "chat"), {
+        message: message,
+      });
+      setMessage("");
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        console.log(e);
+      }
+    }
+  };
+
   return (
     <>
-      <p>チャット</p>
+      <Container py={14}>
+        <Heading>チャット</Heading>
+        <Spacer height={8} aria-hidden />
+        <chakra.form display={"flex"} gap={2} onSubmit={handleSendMessage}>
+          <Input value={message} onChange={(e) => setMessage(e.target.value)} />
+          <Button type={"submit"}>送信</Button>
+        </chakra.form>
+      </Container>
     </>
   );
 };
