85a77da55326b3421952d1443c77de4921b69966
masac <msktjm98@gmail.com>
2023-06-18 10:42:54 +0900

    チャットのデータ取得まで

--- pages/posts/chat.tsx
+++ pages/posts/chat.tsx
@@ -1,20 +1,43 @@
 import {
+  Avatar,
+  Box,
   Button,
   chakra,
   Container,
+  Flex,
   Heading,
   Input,
   Spacer,
+  Text,
 } from "@chakra-ui/react";
 
 import { db } from "@/firebase/firebase";
 import { FirebaseError } from "@firebase/util";
-import { FormEvent, useState } from "react";
-import { addDoc, collection } from "firebase/firestore";
+import { FormEvent, useEffect, useState } from "react";
+import { addDoc, collection, getDoc, onSnapshot } from "firebase/firestore";
+
+const _message = "確認用メッセージです。";
+const _messages = [...Array(10)].map((_, i) => _message.repeat(i + 1));
+
+type MessageProps = {
+  message: string;
+};
+const Message = ({ message }: MessageProps) => {
+  return (
+    <Flex alignItems={"start"}>
+      <Avatar />
+      <Box ml={2}>
+        <Text bgColor={"gray.200"} rounded={"md"} px={2} py={1}>
+          {message}
+        </Text>
+      </Box>
+    </Flex>
+  );
+};
 
 const chat = () => {
+  //追加
   const [message, setMessage] = useState<string>("");
-
   const handleSendMessage = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
@@ -29,11 +52,40 @@ const chat = () => {
     }
   };
 
+  // 取得
+  const [chats, setChats] = useState<{ message: string }[]>([]);
+  useEffect(() => {
+    try {
+      onSnapshot(collection(db, "chat"), (querySnapShot) => {
+        const updatedChats: { message: string }[] = [];
+        querySnapShot.forEach((doc) => {
+          updatedChats.push({ message: doc.data().message });
+        });
+        setChats(updatedChats);
+      });
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        console.error(e);
+      }
+      return;
+    }
+  }, []);
+
   return (
     <>
       <Container py={14}>
         <Heading>チャット</Heading>
-        <Spacer height={8} aria-hidden />
+
+        <Spacer height={4} aria-hidden />
+        <Flex flexDirection={"column"} overflowY={"auto"} gap={2} height={400}>
+          {/* {_messages.map((message, index) => (
+            <Message message={message} key={`ChatMessage_${index}`} />
+          ))} */}
+          {chats.map((chat, index) => (
+            <Message message={chat.message} key={`ChatMessage_${index}`} />
+          ))}
+        </Flex>
+        <Spacer height={2} aria-hidden />
         <chakra.form display={"flex"} gap={2} onSubmit={handleSendMessage}>
           <Input value={message} onChange={(e) => setMessage(e.target.value)} />
           <Button type={"submit"}>送信</Button>
