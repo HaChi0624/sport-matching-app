c48f8be33ba891c2999ba4a57540274e4ea2af75
masac <msktjm98@gmail.com>
2023-07-17 17:05:27 +0900

    refactor

--- pages/posts/friendList.tsx
+++ pages/posts/friendList.tsx
@@ -1,4 +1,12 @@
-import { Container, HStack, Image, Input, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  HStack,
+  Image,
+  Input,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
 import Link from "next/link";
 import goya from "src/goya.png";
 import { useUsers } from "@/hooks/useUsers";
@@ -10,26 +18,27 @@ const friendList = () => {
   // likeのstateがtrueの場合に表示されるようにしたい
   return (
     <>
-      <Container>
+      <Container py="16px" maxW={["90%", "90%", "80%", "70%"]}>
         <Text fontSize="4xl">友達一覧</Text>
         <Input placeholder="検索" />
-        {users.map((user) => (
-          <HStack h="20" key={user.uid}>
-            {user.photoURL ? (
-              <Image src={user.photoURL} alt="picture" w="64px" h="64px" />
-            ) : (
-              <Image src={goya.src} alt="picture" w="64px" h="64px" />
-            )}
-            <Text>
-              <Link href={`/posts/friendProfilePage/${user.uid}`}>
-                {user.userName}
-              </Link>
-              <Link href={`/posts/chat/${user.uid}`}>
-                チャット
-              </Link>
-            </Text>
-          </HStack>
-        ))}
+        <Box mt='8px'>
+          {users.map((user) => (
+            <HStack h="20" key={user.uid}>
+              {user.photoURL ? (
+                <Image src={user.photoURL} alt="picture" w="64px" h="64px" />
+              ) : (
+                <Image src={goya.src} alt="picture" w="64px" h="64px" />
+              )}
+              <Box>
+                <Link href={`/posts/friendProfilePage/${user.uid}`}>
+                  {user.userName}
+                </Link>
+                <Text>最新ログイン：２日前</Text>
+              </Box>
+              <Link href={`/posts/chat/${user.uid}`}>チャット</Link>
+            </HStack>
+          ))}
+        </Box>
       </Container>
     </>
   );
