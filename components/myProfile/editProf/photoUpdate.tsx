7536a7f6e56264b736294d01f5429de4a563e232
masac <msktjm98@gmail.com>
2023-09-10 19:14:06 +0900

    プロフィール画像機能

--- components/myProfile/editProf/photoUpdate.tsx
+++ components/myProfile/editProf/photoUpdate.tsx
@@ -1,42 +1,26 @@
-import { Button, Icon, useDisclosure } from "@chakra-ui/react";
+import { Button, Icon } from "@chakra-ui/react";
 import { SlCamera } from "react-icons/sl";
 
-import { useState } from "react";
-import { doc, updateDoc } from "firebase/firestore";
-
-import { db } from "@/firebase/firebase";
-import { useAuth } from "@/hooks/useAuth";
-import UpdateModal from "./updateModal";
+import usePhotoFile from "@/hooks/usePhotoFile";
 
 //プロフィール写真の更新
 const PhotoUpdate = () => {
-  const { user } = useAuth();
-  const [photoURL, setPhotoURL] = useState("");
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  const handleUpdateProfile = () => {
-    if (user) {
-      updateDoc(doc(db, "users", user.uid), {
-        photoURL: photoURL,
-      });
-    }
-    onClose();
-  };
+  const { hiddenFileInput, handleFileClick, handleFileChange } = usePhotoFile();
 
   return (
     <>
-      <Button onClick={onOpen}>
-        <Icon as={SlCamera} boxSize={"24px"} />
-      </Button>
-
-      <UpdateModal
-        handleUpdateProfile={handleUpdateProfile}
-        value={photoURL}
-        onChange={(e) => setPhotoURL(e.target.value)}
-        isOpen={isOpen}
-        onClose={onClose}
-        placeholder="写真のURL"
-      />
+      <div>
+        <Button onClick={handleFileClick}>
+          <Icon as={SlCamera} boxSize={"24px"} />
+        </Button>
+        <input
+          hidden
+          ref={hiddenFileInput}
+          type="file"
+          //multiple
+          onChange={handleFileChange}
+        />
+      </div>
     </>
   );
 };
---
+++ hooks/usePhotoFile.ts
@@ -0,0 +1,67 @@
+import { useRef, useState } from "react";
+import { db, storage } from "@/firebase/firebase";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { doc, updateDoc } from "firebase/firestore";
+
+import { useAuth } from "./useAuth";
+
+const usePhotoFile = () => {
+  const [progress, setProgress] = useState<number>();
+  const [avatarImage, setAvatarImage] = useState("");
+  // const user = auth.currentUser;
+  const { user } = useAuth();
+
+  const hiddenFileInput = useRef<HTMLInputElement>(null);
+  const handleFileClick = () => {
+    hiddenFileInput.current?.click();
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const file = e.target.files![0];
+    uploadFile(file);
+    return { handleFileChange };
+  };
+
+  const uploadFile = (file: any) => {
+    if (!file) return;
+    const storageRef = ref(storage, `/images/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(progress);
+      },
+      (err) => alert(err.message),
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          console.log(url);
+          setAvatarImage(url); //一度クライアントサイドでステート保持したらそれが表示されるようにしたい
+          //Authユーザーのプロフィール更新（photoURLのみ）
+          if (user) {
+            updateDoc(doc(db, "users", user.uid), {
+              photoURL: url,
+            });
+          }
+        });
+      }
+    );
+  };
+  
+
+  return {
+    progress,
+    avatarImage,
+    setAvatarImage,
+    hiddenFileInput,
+    handleFileClick,
+    handleFileChange,
+    uploadFile,
+  };
+};
+
+export default usePhotoFile;
